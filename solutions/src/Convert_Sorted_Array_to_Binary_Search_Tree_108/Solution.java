1de6acfb30e616bf847fe966e69766853990d7eb
arianaturc <ariana.turc@yahoo.com>
2025-11-15 18:59:54 +0200

    108

---
+++ solutions/src/Convert_Sorted_Array_to_Binary_Search_Tree_108/Solution.java
@@ -0,0 +1,43 @@
+package Convert_Sorted_Array_to_Binary_Search_Tree_108;
+
+
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+/**
+ * [-10, -3, 0, 5, 9]
+ *          0
+ *    -10       5
+ *       -3       9
+ **/
+
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if(left > right)
+            return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+
+        return root;
+
+    }
+}
