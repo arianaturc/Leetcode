ee36c6ad01febfc464f7f2cec7f1602e31ea86ba
arianaturc <ariana.turc@yahoo.com>
2025-11-16 17:13:11 +0200

    70

---
+++ solutions/src/Climbing_Stairs_70/Solution.java
@@ -0,0 +1,20 @@
+package Climbing_Stairs_70;
+
+/// fib
+public class Solution {
+    public int climbStairs(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+
+        int[] arr = new int[50];
+        arr[0] = 1;
+        arr[1] = 2;
+
+        for(int i = 2; i < n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+
+        return arr[n - 1];
+    }
+}
