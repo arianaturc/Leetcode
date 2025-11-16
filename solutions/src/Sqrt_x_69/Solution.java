bb4ef98a1e9acdfac2ec93cf9cafbf148d17fc44
arianaturc <ariana.turc@yahoo.com>
2025-11-16 16:55:18 +0200

    69

---
+++ solutions/src/Sqrt_x_69/Solution.java
@@ -0,0 +1,24 @@
+package Sqrt_x_69;
+
+public class Solution {
+    public int mySqrt(int x) {
+        if(x < 2)
+            return x;
+
+        int left = 1;
+        int right = x / 2;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            long sq = (long)mid * mid;
+            if(sq == x)
+                return mid;
+            if(sq < x) {
+                left = mid + 1;
+            } else
+                right = mid - 1;
+
+        }
+        return right;
+    }
+}
