87622d99872bb4d1418328cdf57e3de34b7aefaf
arianaturc <ariana.turc@yahoo.com>
2025-11-17 18:58:57 +0200

    222

---
+++ solutions/src/Count_Complete_Tree_Nodes_222/Solution.java
@@ -0,0 +1,31 @@
+package Count_Complete_Tree_Nodes_222;
+
+
+
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+  class Solution {
+      int count = 0;
+    public int countNodes(TreeNode root) {
+       if(root == null)
+           return 0;
+
+        countNodes(root.left);
+        count++;
+        countNodes(root.right);
+
+        return count;
+
+    }
+}
