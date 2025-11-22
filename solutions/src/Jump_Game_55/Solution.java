4560cb0f34f606cfd04d1601f8aef361e1aee3b5
arianaturc <ariana.turc@yahoo.com>
2025-11-22 12:06:40 +0200

    55

--- solutions/src/Jump_Game_55/Solution.java
+++ solutions/src/Jump_Game_55/Solution.java
@@ -1,5 +1,14 @@
 package Jump_Game_55;
 
+/// start from the end of the array
+/// first, the target is the last element
+/// then check if from the previous element can get to the target
+/// if so, move the target to the previous element and so on
+/// if the target becomes the first element
+/// the function will return true
+/// else it will return false
+
+
 class Solution {
     public boolean canJump(int[] nums) {
         int len = nums.length;
