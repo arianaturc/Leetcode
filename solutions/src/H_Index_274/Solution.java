d7f76a8f2f251a31f5169a4db39b9f0e2abf8e5c
arianaturc <ariana.turc@yahoo.com>
2025-11-23 12:42:51 +0200

    274

--- solutions/src/H_Index_274/Solution.java
+++ solutions/src/H_Index_274/Solution.java
@@ -1,14 +1,22 @@
 package H_Index_274;
 
+/// use a frequency array with the same length of citations + 1
+/// (the last index used for large values)
+/// populate the freq array
+/// traverse it from the right to left
+/// add the values to the papers variable
+/// if the number of papers are bigger than the index
+/// the h index was found
+/// return the result
+
 public class Solution {
     public int hIndex(int[] citations) {
         int len = citations.length;
         int[] freq = new int[len + 1];
         int papers = 0;
 
-        for(int i = 0; i < len; i++) {
-            int value = citations[i];
-            if(value <= len)
+        for (int value : citations) {
+            if (value <= len)
                 freq[value]++;
             else freq[len]++;
         }
