e68e8081e8dcc407984191623d84d7af4aa14449
arianaturc <ariana.turc@yahoo.com>
2025-11-25 10:13:09 +0200

    238

--- solutions/src/Product_of_Array_Except_Self_238/Solution.java
+++ solutions/src/Product_of_Array_Except_Self_238/Solution.java
@@ -1,5 +1,11 @@
 package Product_of_Array_Except_Self_238;
 
+/// built an array with the products of the elements in the left
+/// and an array with the products of the elements in the right
+/// the value of the left(i) will be p = p * nums(j), j = 0, ..., i - 1
+/// the value of the right(i) will be p = p * nums(j), j = len, ..., i + 1
+/// the elements of the resulted array will be the multiplication of the values from the two arrays
+
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int p = 1;
