b6b1204429aa1494d1e278b28d9767acd9fdbb03
arianaturc <ariana.turc@yahoo.com>
2026-02-15 15:05:07 +0200

    3Sum_15

---
+++ solutions/src/Sum3_15/Solution.java
@@ -0,0 +1,49 @@
+package Sum3_15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/// use two pointers low and high
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            } else if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int low = i + 1;
+            int high = nums.length - 1;
+
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+                    List<Integer> elem = new ArrayList<>();
+                    elem.add(nums[i]);
+                    elem.add(nums[low]);
+                    elem.add(nums[high]);
+
+                    result.add(elem);
+                    low++;
+                    high--;
+
+                    while (low < high && nums[low] == nums[low - 1])
+                        low++;
+                    while (low < high && nums[high] == nums[high + 1])
+                        high--;
+                } else if (sum < 0)
+                    low++;
+                else high--;
+            }
+        }
+        return result;
+
+    }
+}
